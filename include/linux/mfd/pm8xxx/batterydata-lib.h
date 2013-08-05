251d260292e9e85312f15309d7eb1e4eae9a073b
M1cha <sigmaepsilon92@gmail.com>
2013-08-05 10:29:25 +0200

    Revert "power: pm8921-bms: add capacitive resistance"
    This reverts commit 4dc49a5251b0caa0fff8985ce90639714daf889f.

--- include/linux/mfd/pm8xxx/batterydata-lib.h
+++ include/linux/mfd/pm8xxx/batterydata-lib.h
@@ -88,8 +88,6 @@ enum battery_type {
  *			readings from bms are not available.
  * @delta_rbatt_mohm:	the resistance to be added towards lower soc to
  *			compensate for battery capacitance.
- * @rbatt_capacitve_mohm: the resistance to be added to compensate for
- *				battery capacitance
  */
 
 struct bms_battery_data {
@@ -101,7 +99,6 @@ struct bms_battery_data {
 	struct sf_lut		*rbatt_sf_lut;
 	int			default_rbatt_mohm;
 	int			delta_rbatt_mohm;
-	int			rbatt_capacitive_mohm;
 };
 
 #if defined(CONFIG_PM8921_BMS) || \
